304f90d4f31d99b599bf9e27a21e8c0412b3b094
SteveTomorrow <haubodybuilding@icloud.com>
2023-06-16 18:50:44 +0700

    Commit update function Question Game

--- src/question/QuestionSWFrame.java
+++ src/question/QuestionSWFrame.java
@@ -8,6 +8,7 @@ import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -21,79 +22,70 @@ import read.SlangWord;
 import shows.MenuSWFrame;
 
 public class QuestionSWFrame extends JFrame implements ActionListener {
-    JButton b1, b2, b3, b4, btnBack;
-    // final int type;
-    String s[];
+    private JButton answer1, answer2, answer3, answer4, btnBack, btnContinue;
+    private String[] questionData;
+    private int type;
+    private Random random;
+    private JLabel questionLabel;
 
     public QuestionSWFrame(int type) {
-        s = (SlangWord.getInstance()).quiz(type);
-        // A Label
+        this.type = type;
+        random = new Random();
+        questionData = new String[6];
+        generateQuestion();
+
+        // Label
         JLabel label = new JLabel("Choose the correct answer");
         label.setForeground(Color.BLUE);
         label.setFont(new Font("Gill Sans MT", Font.PLAIN, 35));
         label.setAlignmentX(CENTER_ALIGNMENT);
         label.setAlignmentY(-100);
-        JLabel questionLabel;
-        if (type == 1)
-            questionLabel = new JLabel("What does Slang " + "`" + s[0] + "`" + "mean?");
-        else
-            questionLabel = new JLabel("`" + s[0] + "`" + " has Slang Word is?");
+        questionLabel = createQuestionLabel();
         questionLabel.setForeground(Color.black);
         questionLabel.setFont(new Font("Gill Sans MT", Font.PLAIN, 18));
         questionLabel.setAlignmentX(CENTER_ALIGNMENT);
         questionLabel.setAlignmentY(-100);
 
-        // Add space
-        // A Grid Answers
-        b1 = new JButton("A." + s[1]);
-        b1.addActionListener(this);
-//        b1.setBackground(Color.green);
-        b1.setFont(new Font("Lucida Handwriting", Font.PLAIN, 14));
-        b1.setForeground(Color.red);
-        b2 = new JButton("B." + s[2]);
-        b2.addActionListener(this);
-//        b2.setBackground(Color.green);
-        b2.setFont(new Font("Lucida Handwriting", Font.PLAIN, 14));
-        b2.setForeground(Color.red);
-        b3 = new JButton("C." + s[3]);
-        b3.addActionListener(this);
-        b3.setFont(new Font("Lucida Handwriting", Font.PLAIN, 14));
-        b3.setForeground(Color.red);
-//        b3.setBackground(Color.green);
-        b4 = new JButton("D. " + s[4]);
-        b4.addActionListener(this);
-        b4.setFont(new Font("Lucida Handwriting", Font.PLAIN, 14));
-        b4.setForeground(Color.red);
-//        b4.setBackground(Color.green);
+        // Grid Answers
+        answer1 = createAnswerButton(questionData[1]);
+        answer2 = createAnswerButton(questionData[2]);
+        answer3 = createAnswerButton(questionData[3]);
+        answer4 = createAnswerButton(questionData[4]);
 
         JPanel panelCenter = new JPanel();
         panelCenter.setLayout(new GridLayout(2, 2, 10, 10));
-        panelCenter.add(b1);
-        panelCenter.add(b2);
-        panelCenter.add(b3);
-        panelCenter.add(b4);
+        panelCenter.add(answer1);
+        panelCenter.add(answer2);
+        panelCenter.add(answer3);
+        panelCenter.add(answer4);
         Dimension size2 = new Dimension(600, 200);
         panelCenter.setMaximumSize(size2);
         panelCenter.setPreferredSize(size2);
         panelCenter.setMinimumSize(size2);
-        // Button back
 
+        // Buttons
         btnBack = new JButton("Back");
         btnBack.addActionListener(this);
 
+        btnContinue = new JButton("Continue");
+        btnContinue.addActionListener(this);
+        btnContinue.setEnabled(false);
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(btnBack);
+        buttonPanel.add(btnContinue);
 
-        Container con = this.getContentPane();
-        con.setLayout(new BoxLayout(con, BoxLayout.Y_AXIS));
-        con.add(Box.createRigidArea(new Dimension(0, 10)));
-        con.add(label);
-        con.add(Box.createRigidArea(new Dimension(0, 30)));
-        con.add(questionLabel);
-        con.add(Box.createRigidArea(new Dimension(0, 50)));
-        con.add(panelCenter);
-        con.add(Box.createRigidArea(new Dimension(0, 50)));
-        con.add(buttonPanel);
+        Container container = this.getContentPane();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        container.add(Box.createRigidArea(new Dimension(0, 10)));
+        container.add(label);
+        container.add(Box.createRigidArea(new Dimension(0, 30)));
+        container.add(questionLabel);
+        container.add(Box.createRigidArea(new Dimension(0, 50)));
+        container.add(panelCenter);
+        container.add(Box.createRigidArea(new Dimension(0, 50)));
+        container.add(buttonPanel);
+
         this.setTitle("Question Quiz");
         this.setVisible(true);
         this.setSize(700, 500);
@@ -103,59 +95,109 @@ public class QuestionSWFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        if (e.getSource() == b1) {
-            this.answer(1);
-        } else if (e.getSource() == b2) {
-            this.answer(2);
-        } else if (e.getSource() == b3) {
-            this.answer(3);
-        } else if (e.getSource() == b4) {
-            this.answer(4);
+        if (e.getSource() == answer1) {
+            checkAnswer(answer1);
+        } else if (e.getSource() == answer2) {
+            checkAnswer(answer2);
+        } else if (e.getSource() == answer3) {
+            checkAnswer(answer3);
+        } else if (e.getSource() == answer4) {
+            checkAnswer(answer4);
         } else if (e.getSource() == btnBack) {
             this.dispose();
-            new MenuSWFrame();        }
+            new MenuSWFrame();
+        } else if (e.getSource() == btnContinue) {
+            resetQuestion();
+        }
     }
 
-    public void answer(int ans) {
-        if (s[ans].equals(s[5])) {
-            // default title and icon
-            JOptionPane.showMessageDialog(this, "Correct Answer.");
+    private void checkAnswer(JButton selectedAnswer) {
+        disableAllButtons();
+        String selectedText = selectedAnswer.getText().substring(3); // Remove the prefix "A. ", "B. ", "C. ", or "D. "
+        if (selectedText.equals(questionData[5])) {
+            selectedAnswer.setBackground(Color.GREEN);
+            JOptionPane.showMessageDialog(this, "Correct Answer!");
+        } else {
+            selectedAnswer.setBackground(Color.RED);
+            JOptionPane.showMessageDialog(this, "Wrong Answer!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        showCorrectAnswer();
+        btnContinue.setEnabled(true);
+    }
 
-        } else {
-            JOptionPane.showMessageDialog(this, "Wrong Answer", "Inane error", JOptionPane.ERROR_MESSAGE);
-            if (ans == 1)
-                b1.setBackground(Color.red);
-            else if (ans == 2)
-                b2.setBackground(Color.red);
-            else if (ans == 3)
-                b3.setBackground(Color.red);
-            else if (ans == 4)
-                b4.setBackground(Color.red);
+    private void showCorrectAnswer() {
+        if (questionData[1].equals(questionData[5])) {
+            answer1.setBackground(Color.YELLOW);
         }
-        if (s[1].equals(s[5])) {
-            b1.setEnabled(false);
-            b1.setBackground(Color.yellow);
-        } else {
-            b1.setEnabled(false);
+        else if (questionData[2].equals(questionData[5])) {
+            answer2.setBackground(Color.YELLOW);
         }
-        if (s[2].equals(s[5])) {
-            b2.setEnabled(false);
-            b2.setBackground(Color.yellow);
-        } else {
-            b2.setEnabled(false);
+        else if (questionData[3].equals(questionData[5])) {
+            answer3.setBackground(Color.YELLOW);
         }
-        if (s[3].equals(s[5])) {
-            b3.setEnabled(false);
-            b3.setBackground(Color.yellow);
-        } else {
-            b3.setEnabled(false);
+        else if (questionData[4].equals(questionData[5])) {
+            answer4.setBackground(Color.YELLOW);
         }
-        if (s[4].equals(s[5])) {
-            b4.setEnabled(false);
-            b4.setBackground(Color.yellow);
+    }
+
+    private void disableAllButtons() {
+        answer1.setEnabled(false);
+        answer2.setEnabled(false);
+        answer3.setEnabled(false);
+        answer4.setEnabled(false);
+    }
+
+    private void generateQuestion() {
+        questionData = SlangWord.getInstance().quiz(type);
+    }
+
+    private void resetQuestion() {
+        resetButtonColors();
+        enableAllButtons();
+        generateQuestion();
+        updateQuestionLabel();
+        updateAnswerButtons();
+        btnContinue.setEnabled(false);
+    }
+
+    private void resetButtonColors() {
+        answer1.setBackground(null);
+        answer2.setBackground(null);
+        answer3.setBackground(null);
+        answer4.setBackground(null);
+    }
+
+    private void enableAllButtons() {
+        answer1.setEnabled(true);
+        answer2.setEnabled(true);
+        answer3.setEnabled(true);
+        answer4.setEnabled(true);
+    }
+
+    private void updateQuestionLabel() {
+        questionLabel.setText("`" + questionData[0] + "`" + " has Slang Word is?");
+    }
+
+    private void updateAnswerButtons() {
+        answer1.setText("A. " + questionData[1]);
+        answer2.setText("B. " + questionData[2]);
+        answer3.setText("C. " + questionData[3]);
+        answer4.setText("D. " + questionData[4]);
+    }
+
+    private JButton createAnswerButton(String answerText) {
+        JButton button = new JButton(answerText);
+        button.addActionListener(this);
+        button.setFont(new Font("Lucida Handwriting", Font.PLAIN, 14));
+        button.setForeground(Color.red);
+        return button;
+    }
+
+    private JLabel createQuestionLabel() {
+        if (type == 1) {
+            return new JLabel("What does Slang " + "`" + questionData[0] + "`" + " mean?");
         } else {
-            b4.setEnabled(false);
+            return new JLabel("`" + questionData[0] + "`" + " has Slang Word is?");
         }
     }
 }
